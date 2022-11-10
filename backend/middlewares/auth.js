491fca20089f80143d7090d5fe26b50451eef046
unknown <romanovainnap@yandex.ru>
2022-11-10 19:07:01 +0300

    console log cookies

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,15 +5,16 @@ const UnauthorixedErrorCode = require('../errors/unauthorixedErrorCode');
 module.exports = (req, _, next) => {
   const { token } = req.cookies;
   if (!token) {
-    throw new UnauthorixedErrorCode('Необходима авторизация');
+    throw new UnauthorixedErrorCode('Необходима авторизация 1');
   }
 
   let payload;
   // верифицирую токен
   try {
     payload = jwt.verify(token, getJWTSecretKey());
+    console.log(payload);
   } catch (err) {
-    return next(new UnauthorixedErrorCode('Необходима авторизация'));
+    return next(new UnauthorixedErrorCode('Необходима авторизация 2'));
     // отправляю ошибку, если не получилось
   }
   // записываю пейлоуд в объект запроса
