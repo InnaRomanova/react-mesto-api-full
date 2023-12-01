bfb1b397af9f3371108ce1be68b8fed4d2c5451c
unknown <romanovainnap@yandex.ru>
2023-12-01 13:44:31 +0300

    auth,js file is backend edit

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,22 +3,23 @@ const { getJWTSecretKey } = require('../utils/utils');
 const UnauthorixedErrorCode = require('../errors/unauthorixedErrorCode');
 
 module.exports = (req, _, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer')) {
-    throw new UnauthorixedErrorCode('Необходима авторизация 1');
-  }
-  const token = authorization.replace('Bearer ', '');
-  // const { token } = req.cookies;
-  // if (!token) {
-  //   throw new UnauthorixedErrorCode('Необходима авторизация');
+  // const { authorization } = req.headers;
+  // if (!authorization || !authorization.startsWith('Bearer')) {
+  //   throw new UnauthorixedErrorCode('Необходима авторизация 1');
   // }
+  // const token = authorization.replace('Bearer ', '');
+
+  const { token } = req.cookies;
+  if (!token) {
+    throw new UnauthorixedErrorCode('Необходима авторизация2');
+  }
 
   let payload;
   // верифицирую токен
   try {
     payload = jwt.verify(token, getJWTSecretKey());
   } catch (err) {
-    return next(new UnauthorixedErrorCode('Необходима авторизация 2'));
+    return next(new UnauthorixedErrorCode('Необходима авторизация3'));
     // отправляю ошибку, если не получилось
   }
   // записываю пейлоуд в объект запроса
